650f284a789659c15d69b418938a360964c7fc46
roitblatari <roitblatari@gmail.com>
2019-07-30 11:41:24 -0400

    current_user

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+ 
+
+  def current_user
+    if User.find_by_id(session[:user_id])
+      @current_user ||= User.find_by_id(session[:user_id])
+    else
+      nil
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+ 
+
 end
