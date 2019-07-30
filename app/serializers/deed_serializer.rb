3eca0b6d1532524788eebdd97b5caac09328c709
roitblatari <roitblatari@gmail.com>
2019-07-30 11:42:10 -0400

    serializer

---
+++ app/serializers/deed_serializer.rb
@@ -0,0 +1,6 @@
+class DeedSerializer < ActiveModel::Serializer
+  belongs_to :giver,    foreign_key: "giver_id",    class_name: "User" 
+  belongs_to :receiver, foreign_key: "receiver_id", class_name: "User", serializer: UserSerializer
+
+  attributes :id, :title, :content, :image_url, :giver_id, :receiver_id 
+end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,10 @@
+class UserSerializer < ActiveModel::Serializer
+  has_many :given_deeds,    foreign_key: "giver_id",    class_name: "Deed"
+  # # has_many :received_deeds, foreign_key: "receiver_id", class_name: "Deed" 
+
+  
+  has_many :givers, through: :given_deeds
+  # has_many :receivers, through: :received_deeds
+
+  attributes :id, :username, :email, :image_url
+end
