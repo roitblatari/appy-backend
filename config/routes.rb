92c35bbca884085258fa715104221deddd9b6a90
roitblatari <roitblatari@gmail.com>
2019-07-30 11:38:28 -0400

    nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :deeds
-  resources :users
+
+  get 'sessions/new'
+  namespace :api do
+    namespace :v1 do 
+      resources :users
+      resources :deeds
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
