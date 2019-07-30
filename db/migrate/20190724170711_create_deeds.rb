8f38fce2a2c04ec6b37c59c66e32a1da103f7dd8
roitblatari <roitblatari@gmail.com>
2019-07-30 11:43:50 -0400

    change user_giver_id to giver_id

--- db/migrate/20190724170711_create_deeds.rb
+++ db/migrate/20190724170711_create_deeds.rb
@@ -2,8 +2,8 @@ class CreateDeeds < ActiveRecord::Migration[5.2]
   def change
     create_table :deeds do |t|
       t.string :title
-      t.string :user_giver_id
-      t.string :user_receiver_id
+      t.string :giver_id
+      t.string :receiver_id
       t.string :content
       t.string :image_url
 
