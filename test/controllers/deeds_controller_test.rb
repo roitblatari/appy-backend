fa5a01798a1c8978528baf852feb439e5128e283
roitblatari <roitblatari@gmail.com>
2019-07-30 11:44:45 -0400

    .

--- test/controllers/deeds_controller_test.rb
+++ test/controllers/deeds_controller_test.rb
@@ -12,7 +12,7 @@ class DeedsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create deed" do
     assert_difference('Deed.count') do
-      post deeds_url, params: { deed: { content: @deed.content, image_url: @deed.image_url, title: @deed.title, user_giver_id: @deed.user_giver_id, user_receiver_id: @deed.user_receiver_id } }, as: :json
+      post deeds_url, params: { deed: { content: @deed.content, image_url: @deed.image_url, title: @deed.title, giver_id: @deed.giver_id, receiver_id: @deed.receiver_id } }, as: :json
     end
 
     assert_response 201
