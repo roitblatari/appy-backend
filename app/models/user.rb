090963eaf6c913374a047e403e389151ded862e5
roitblatari <roitblatari@gmail.com>
2019-07-30 11:40:09 -0400

    user model  and has many ...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  has_many :given_deeds,    foreign_key: "giver_id",    class_name: "Deed"
+  has_many :received_deeds, foreign_key: "receiver_id", class_name: "Deed" 
+
+  has_many :givers, through: :given_deeds
+  has_many :receivers, through: :received_deeds
+
   has_secure_password
+
 end
