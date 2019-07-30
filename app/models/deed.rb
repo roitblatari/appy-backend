7b5d5f6c28c8b31421150d664eadb504a63daa64
roitblatari <roitblatari@gmail.com>
2019-07-30 11:39:19 -0400

    deed model

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,2 +1,15 @@
-class Deed < ApplicationRecord
+class Deed < ActiveRecord::Base
+  belongs_to :giver,    foreign_key: "giver_id",    class_name: "User" 
+  belongs_to :receiver, foreign_key: "receiver_id", class_name: "User"
 end
+# class Deed < ApplicationRecord
+#   belongs_to :user_giver, class_name: "User"
+#   belongs_to :user_receiver, class_name: "User"
+# end
+
+
+# app/models/deeds.rb
+# class Deed < ActiveRecord::Base
+#   belongs_to :giver,    foreign_key: :giver_id,    class_name: "User" 
+#   belongs_to :receiver, foreign_key: :receiver_id, class_name: "User"
+# end
