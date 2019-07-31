1c87bb47c503d5bf6c1f70e3ff5cd7ddade28017
roitblatari <roitblatari@gmail.com>
2019-07-31 14:31:01 -0400

    fastjson api

--- app/serializers/deed_serializer.rb
+++ app/serializers/deed_serializer.rb
@@ -1,6 +1,8 @@
-class DeedSerializer < ActiveModel::Serializer
+class DeedSerializer #< ActiveModel::Serializer
+  include FastJsonapi::ObjectSerializer
+
   belongs_to :giver,    foreign_key: "giver_id",    class_name: "User" 
-  belongs_to :receiver, foreign_key: "receiver_id", class_name: "User", serializer: UserSerializer
+  belongs_to :receiver, foreign_key: "receiver_id", class_name: "User" #, serializer: UserSerializer
 
   attributes :id, :title, :content, :image_url, :giver_id, :receiver_id 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,11 @@
-class UserSerializer < ActiveModel::Serializer
-  has_many :given_deeds,    foreign_key: "giver_id",    class_name: "Deed"
-  # # has_many :received_deeds, foreign_key: "receiver_id", class_name: "Deed" 
+class UserSerializer #< ActiveModel::Serializer
+  include FastJsonapi::ObjectSerializer
+  # has_many :given_deeds,    foreign_key: "giver_id",    class_name: "Deed"
+  # has_many :received_deeds, foreign_key: "receiver_id", class_name: "Deed" 
 
   
-  has_many :givers, through: :given_deeds
+  # has_many :givers, through: :given_deeds
   # has_many :receivers, through: :received_deeds
 
-  attributes :id, :username, :email, :image_url
+  attributes :id, :username, :email, :image_url, :givers, :receivers, :given_deeds 
 end
