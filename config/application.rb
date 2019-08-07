d4cf6b35a4313f8132d296058c1d1711f9f72164
roitblatari <roitblatari@gmail.com>
2019-08-07 19:46:50 -0400

    clear cookeis

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module AppyBackend
     config.api_only = true
 
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name' 
+    config.middleware.use ActionDispatch::Session::CookieStore #, key: '_cookie_name' 
     
   end
 end
