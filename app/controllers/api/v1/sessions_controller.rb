80395fefd99a81caedd5112fcfd4c9e129bc0fb4
roitblatari <roitblatari@gmail.com>
2019-08-07 19:47:53 -0400

    nothing

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   # def new
   # end
    def create
+    # binding.pry 
     @user = User.find_by(username: params[:session][:username])
 
     if @user && @user.authenticate(params[:session][:password])
@@ -30,4 +31,7 @@ class Api::V1::SessionsController < ApplicationController
       notice: "successfully logged out"
     }, status: :ok
   end
+
+
+
 end
