fea9cbbaec6858f6fb2cb944b86f8734451eb06e
roitblatari <roitblatari@gmail.com>
2019-08-07 19:47:25 -0400

    log out route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
   post '/api/v1/login', to: 'api/v1/sessions#create'
+  delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
   namespace :api do
     namespace :v1 do 
       resources :users
