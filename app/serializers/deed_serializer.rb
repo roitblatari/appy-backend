ac6e9551757e9fa0236f74cd55c2b5164c5f0093
roitblatari <roitblatari@gmail.com>
2019-09-20 11:38:49 -0400

    nothing

--- app/serializers/deed_serializer.rb
+++ app/serializers/deed_serializer.rb
@@ -1,7 +1,7 @@
 class DeedSerializer #< ActiveModel::Serializer
   include FastJsonapi::ObjectSerializer
 
-  belongs_to :giver,    foreign_key: "giver_id",    class_name: "User" 
+  belongs_to :giver,    foreign_key: "giver_id",    class_name: "User" #, serializer: UserSerializer
   belongs_to :receiver, foreign_key: "receiver_id", class_name: "User" #, serializer: UserSerializer
 
   attributes :id, :title, :content, :image_url, :giver_id, :receiver_id 
