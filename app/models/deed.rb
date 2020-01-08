a7ce16d15041885291098d5ac06222fa1f993610
roitblatari <roitblatari@gmail.com>
2020-01-08 13:06:40 -0500

    Looking into more validations

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,6 +1,11 @@
 class Deed < ActiveRecord::Base
+  # include ActiveModel::Validations
   belongs_to :giver,    foreign_key: "giver_id",    class_name: "User" 
   belongs_to :receiver, foreign_key: "receiver_id", class_name: "User"
 
   scope :display_deeds, ->  { where("created_at > ?", 40.days.ago) }
+
+  # validates! :title, presence: true
+  # validates! :content, presence: true
+  
 end
